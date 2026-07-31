e8253eb616b089f52fc5a7517e70a483672e5850
Tarunkumar Macchi <tarunmachhi29@gmail.com>
2026-07-31 13:55:11 +0530

    Time: 0 ms (100%), Space: 41.8 MB (93.89%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,15 +1,46 @@
 class Solution {
+    int[] memo;
+
     public int fib(int n) {
+        memo = new int[n + 1];
+        return solve(n);
+    }
+
+    int solve(int n) {
         if (n <= 1) return n;
 
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
+        if (memo[n] != 0)
+            return memo[n];
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-
-        return dp[n];
+        memo[n] = solve(n - 1) + solve(n - 2);
+        return memo[n];
     }
 }
+
+
+
+// class Solution {
+//     public int fib(int n) {
+//         if (n == 0) return 0;
+
+//         if (n == 1) return 1;
+
+//         return fib(n - 1) + fib(n - 2);
+//     }
+// }
+
+// class Solution {
+//     public int fib(int n) {
+//         if (n <= 1) return n;
+
+//         int[] dp = new int[n + 1];
+//         dp[0] = 0;
+//         dp[1] = 1;
+
+//         for (int i = 2; i <= n; i++) {
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
+
+//         return dp[n];
+//     }
+// }
