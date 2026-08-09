1cdd220e083c84c55b515f13f37bf387d3072467
Tarunkumar Macchi <tarunmachhi29@gmail.com>
2026-08-09 10:33:04 +0530

    Time: 1 ms (100%), Space: 44 MB (91.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,12 +4,26 @@ class Solution {
         int right = s.length() - 1;
 
         while(left <= right){
-            while(left <= right && !Character.isLetterOrDigit(s.charAt(left))) left++;
-            while(left <= right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            char lchar = s.charAt(left);
+            char rchar = s.charAt(right);
 
-            if(left > right) break;
+            if(lchar >= 'A' && lchar <= 'Z'){
+                lchar = (char) (lchar + 32);
+            }else if(!((lchar >= 'a' && lchar <= 'z') || (lchar >= '0' && lchar <= '9'))){
+                left++;
+                continue;
+            }
 
-            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
+            if(rchar >= 'A' && rchar <= 'Z'){
+                rchar = (char) (rchar + 32);
+            }else if(!((rchar >= 'a' && rchar <= 'z') || (rchar >= '0' && rchar <= '9'))){
+                right--;
+                continue;
+            }
+
+            if(lchar != rchar) {
+                return false;
+            }
 
             left++;
             right--;
