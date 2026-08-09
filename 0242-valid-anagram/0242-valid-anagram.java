de90fb88da57c21d93446a8644b13cd2fddf0620
Tarunkumar Macchi <tarunmachhi29@gmail.com>
2026-08-09 09:21:24 +0530

    Time: 6 ms (43.02%), Space: 44.6 MB (56.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,12 +5,14 @@ class Solution {
         }
 
         int[] ans = new int[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            ans[s.charAt(i) - 'a']++; 
-            ans[t.charAt(i) - 'a']--;
+        
+        for(int i = 0; i < s.length(); i++){
+            ans[s.charAt(i) - 'a']++;
         }
 
+        for(int i = 0; i < t.length(); i++){
+            ans[t.charAt(i) - 'a']--;
+        }
 
         return Arrays.stream(ans).allMatch(x -> x == 0);
     }
