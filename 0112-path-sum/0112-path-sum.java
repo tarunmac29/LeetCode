c0d4f8a6f4e55c457e3eb1460bd03fc9246af602
Tarunkumar Macchi <tarunmachhi29@gmail.com>
2026-08-22 07:46:00 +0530

    Time: 0 ms (100%), Space: 44.9 MB (50.37%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -15,9 +15,7 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root == null){
-            return false;
-        }
+        if(root == null) return false;
 
         targetSum -= root.val;
         if(root.left == null && root.right == null){
@@ -25,7 +23,6 @@ class Solution {
                 return true;
             }
         }
-
 
         return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
     }
