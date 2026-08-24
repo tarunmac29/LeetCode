2a7c59a9d50bb75e065292f0f9ed2edda75eb98d
Tarunkumar Macchi <tarunmachhi29@gmail.com>
2026-08-24 08:30:00 +0530

    Time: 0 ms (100%), Space: 42.7 MB (71.74%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -18,6 +18,7 @@ class Solution {
         if(p == q) return true;
 
         if(p == null || q == null) return false;
+
         return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
