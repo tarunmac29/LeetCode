3c394369419e57eb015f50bf183aae1a6d817525
Tarunkumar Macchi <tarunmachhi29@gmail.com>
2026-08-26 08:48:12 +0530

    Time: 34 ms (59.72%), Space: 59.2 MB (59.8%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,7 +5,6 @@ class Solution {
             return new ArrayList<>();
         }
         Arrays.sort(nums);
-
         List<List<Integer>> ans = new ArrayList<>();
 
         for(int i = 0; i < nums.length - 2; i++){
@@ -13,30 +12,26 @@ class Solution {
 
             int low = i + 1;
             int high = nums.length - 1;
+
             while(low < high){
                 int sum = nums[i] + nums[low] + nums[high];
 
                 if(sum == 0){
                     ans.add(Arrays.asList(nums[i], nums[low], nums[high]));
 
-                    while(low < high && nums[low] == nums[low + 1]){
-                        low++;
-                    }
-
-                    while(low < high && nums[high] == nums[high - 1]){
-                        high--;
-                    }
+                    while(low < high && nums[low] == nums[low+1]) low++;
+                    while(low < high && nums[high] == nums[high-1]) high--;
 
                     low++;
                     high--;
-                }else if(sum < 0){
-                    low++;
+                }else if(sum > 0){
+                    high--;
                 }else{
-                    high--;
-                }      
+                    low++;
+                }
             }
-        }
 
+        }
         return ans;
     }
 }
