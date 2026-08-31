b6b1dc6351ef00fc08d4194eeb5e65c1fa2a9c32
Tarunkumar Macchi <tarunmachhi29@gmail.com>
2026-08-31 12:01:38 +0530

    Time: 19 ms (83.18%), Space: 45.6 MB (77.57%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -18,24 +18,24 @@ class Solution {
                 continue;
             }
             
-            if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
-                break; 
-            }
-            if ((long) nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) {
-                continue; 
-            }
+            // if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+            //     break; 
+            // }
+            // if ((long) nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) {
+            //     continue; 
+            // }
             
             for (int j = i + 1; j < n - 2; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) {
                     continue;
                 }
                 
-                if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
-                    break;
-                }
-                if ((long) nums[i] + nums[j] + nums[n - 1] + nums[n - 2] < target) {
-                    continue;
-                }
+                // if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                //     break;
+                // }
+                // if ((long) nums[i] + nums[j] + nums[n - 1] + nums[n - 2] < target) {
+                //     continue;
+                // }
                 
                 int left = j + 1;
                 int right = n - 1;
