5a0d65a673c69350918bbe1fface1bf00c1e50b1
Tarunkumar Macchi <tarunmachhi29@gmail.com>
2026-09-03 14:32:22 +0530

    Time: 1 ms (70.95%), Space: 43.7 MB (50.93%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,40 +13,50 @@
  *     }
  * }
  */
-class Tuple {
-    TreeNode node;
-    int vertical;
+// class Tuple {
+//     TreeNode node;
+//     int vertical;
 
-    Tuple(TreeNode node, int vertical) {
-        this.node = node;
-        this.vertical = vertical;
-    }
-}
+//     Tuple(TreeNode node, int vertical) {
+//         this.node = node;
+//         this.vertical = vertical;
+//     }
+// }
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         if(root == null) return ans;
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        // TreeMap<Integer, Integer> map = new TreeMap<>();
 
-        Queue<Tuple> que = new ArrayDeque<>();
+        Queue<TreeNode> que = new ArrayDeque<>();
+        // Queue<Tuple> que = new ArrayDeque<>();
 
-        que.offer(new Tuple(root, 0));
+        // que.offer(new Tuple(root, 0));
+        que.offer(root);
 
         while(!que.isEmpty()){
-            Tuple tuple = que.poll();
+            // Tuple tuple = que.poll();
 
-            TreeNode node = tuple.node;
-            int x = tuple.vertical;
+            // TreeNode node = tuple.node;
+            // int x = tuple.vertical;
 
-            map.put(x, node.val);
+            // map.put(x, node.val);
+            int level = que.size();
 
-            if(node.left != null) que.offer(new Tuple(node.left, x+1));
-            if(node.right != null) que.offer(new Tuple(node.right, x+1));
+            for(int i = 0; i < level; i++){
+                TreeNode node = que.poll();
+                if(i == level - 1) ans.add(node.val);
+
+                if(node.left != null) que.offer(node.left);
+                if(node.right != null) que.offer(node.right);
+            }
+            // if(node.left != null) que.offer(new Tuple(node.left, x+1));
+            // if(node.right != null) que.offer(new Tuple(node.right, x+1));
         }
 
-        for(int val : map.values()){
-            ans.add(val);
-        }
+        // for(int val : map.values()){
+        //     ans.add(val);
+        // }
 
         return ans;
     }
