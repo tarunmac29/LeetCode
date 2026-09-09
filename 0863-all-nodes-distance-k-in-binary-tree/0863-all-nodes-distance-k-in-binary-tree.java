d75834ccbf89d725f9c4549cad94540564e9e0ec
Tarunkumar Macchi <tarunmachhi29@gmail.com>
2026-09-09 16:06:52 +0530

    Time: 15 ms (9.18%), Space: 43.4 MB (98.26%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public static void markParent(TreeNode root, HashMap<TreeNode,TreeNode> pt){
+        Queue<TreeNode> que = new ArrayDeque<>();
+
+        que.offer(root);
+
+        while(!que.isEmpty()){
+            TreeNode node = que.poll();
+
+            if(node.left != null){
+                que.offer(node.left);
+                pt.put(node.left, node);
+            }
+
+            if(node.right != null){
+                que.offer(node.right);
+                pt.put(node.right, node);
+            }
+        }
+    }
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        HashMap<TreeNode, TreeNode> pt = new HashMap<>();
+        markParent(root, pt);
+
+        HashMap<TreeNode, Boolean> visited = new HashMap<>();
+
+        Queue<TreeNode> que = new ArrayDeque<>();
+
+        que.offer(target);
+        visited.put(target, true);
+
+        int cur_level = 0;
+
+        while(!que.isEmpty()){
+            int size = que.size();
+
+            if(cur_level == k) break;
+
+            cur_level++;
+
+            for(int i = 0; i < size; i++){
+                TreeNode node = que.poll();
+
+                if(node.left != null && visited.get(node.left) == null){
+                    que.offer(node.left);
+                    visited.put(node.left, true);
+                }
+                if(node.right != null && visited.get(node.right) == null){
+                    que.offer(node.right);
+                    visited.put(node.right, true);
+                }
+
+                if(pt.get(node) != null && visited.get(pt.get(node)) == null){
+                    que.offer(pt.get(node));
+                    visited.put(pt.get(node), true);
+                }
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        while(!que.isEmpty()){
+            ans.add(que.poll().val);
+        }
+
+        return ans;
+
+    }
+}
