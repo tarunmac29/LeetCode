a18435d5868c32a73fb55d848a1b2df01a1cb53c
Tarunkumar Macchi <tarunmachhi29@gmail.com>
2026-09-10 10:52:52 +0530

    Time: 0 ms (100%), Space: 45.7 MB (84.21%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
@@ -1,53 +1,22 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    private static class Result {
-        TreeNode node;
-        int depth;
-
-        Result(TreeNode node, int depth) {
-            this.node = node;
-            this.depth = depth;
-        }
-    }
+    TreeNode res;
+    int maxDepth = -1;
 
     public TreeNode lcaDeepestLeaves(TreeNode root) {
-        return dfs(root).node;
+        dfs(root, 0);
+        return res;
     }
 
-    private Result dfs(TreeNode node) {
+    private int dfs(TreeNode node, int depth) {
         if (node == null) {
-            return new Result(null, 0);
+            maxDepth = Math.max(maxDepth, depth);
+            return depth;
         }
 
-        Result left = dfs(node.left);
-        Result right = dfs(node.right);
-
-        // If both subtrees have the same max depth, this current node 
-        // is the deepest common ancestor for the leaves below it.
-        if (left.depth == right.depth) {
-            return new Result(node, left.depth + 1);
-        }
-
-        // If one side is deeper, it contains the target LCA. 
-        // Propagate that side's LCA up with the incremented depth.
-        if (left.depth > right.depth) {
-            return new Result(left.node, left.depth + 1);
-        } else {
-            return new Result(right.node, right.depth + 1);
-        }
+        int left = dfs(node.left, depth + 1);
+        int right = dfs(node.right, depth + 1);
+        if (left == right && left == maxDepth)
+            res = node;
+        return Math.max(left, right);
     }
 }
